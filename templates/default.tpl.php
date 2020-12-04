b3129e09b6f7a4c1b191856fe271b05c32b287d5
Matheus G. Dias <matheusgiga123@gmail.com>
2020-12-03 23:09:55 -0300

    Add bootstrap

--- templates/default.tpl.php
+++ templates/default.tpl.php
@@ -6,48 +6,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-    * {
-        margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-
-    nav {
-        width: 100%;
-        background-color: #4a7188;
-
-        display: flex;
-        list-style: none;
-    }
-
-    li {
-        padding: 6px 12px;
-    }
-
-    a {
-        color: #fff;
-        text-decoration: none;
-        font-size: 17px;
-    }
-
-    .content {
-        padding: 20px;
-    }
+    <?php include 'css/style.css';
+    ?>
     </style>
 </head>
 
-<body>
-    <nav>
-        <li>
-            <a href="index.php/home">Home</a>
-        </li>
-        <li>
-            <a href="index.php/admin">Admin</a>
-        </li>
-    </nav>
-    <div class="content">
-        <?php include $content?>
+<body class="d-flex flex-column">
+    <div class="header">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="./home">Painel Admin</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./admin">Admin</a>
+                </li>
+            </ul>
+        </nav>
     </div>
+
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50">
+                            MENU
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">
+                            Páginas
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            Usuários
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-10" id="content">
+                <?php include $content?>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
